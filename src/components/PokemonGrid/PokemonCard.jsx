44e810077bd144603f32c11bacc09c0643bdddb7
Luis Hernandez <luis@heysparkbox.com>
2021-06-08 23:11:18 -0400

    feat: add icon to encode favorite status

--- src/components/PokemonGrid/PokemonCard.jsx
+++ src/components/PokemonGrid/PokemonCard.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { AiFillHeart } from "react-icons/ai";
+import "./PokemonCard/PokemonCard.scss";
 
 function PokemonCard({ pokemonDetails, actions, favorite }) {
   const { name, url } = pokemonDetails;
   const { favoriteAPokemon, unfavoriteAPokemon } = actions;
 
   return (
-    <div className="pokemonCard">
-      <h2 className="pokemonCard__name">{name}</h2>
+    <div className={`pokemonCard ${favorite && "pokemonCard--favorite"}`}>
+      <h2 className="pokemonCard__name">
+        {name} <span> {favorite && <AiFillHeart />}</span>
+      </h2>
       <p className="pokemonCard__url">{url}</p>
       <div className="pokemonCard__actions">
         <button onClick={() => favoriteAPokemon(pokemonDetails)}>
           Favorite
         </button>
         <button onClick={() => unfavoriteAPokemon(url)}>Unfavorite</button>
-        {favorite && "im a favorite"}
       </div>
     </div>
   );
